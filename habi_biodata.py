c3788272d1774a63b61071ea258e4f6bb5d387be
Habi Joseph <habijose@gmail.com>
2020-11-03 12:24:56 +0300

    Ï have added a python scropt that prints my biodata

---
+++ habi_biodata.py
@@ -0,0 +1,19 @@
+def my_biodata():
+
+    first_name = "Habi"
+    last_name = "Joseph"
+    course = "Bsc. Biomedical"
+    year_of_study = 3
+    college ="Health Sciences"
+    university = "Makerere University Kampala"
+
+
+    print("First Name: {}\nLast Name: {}\nCourse: {}\nYear of study: {}\nCollege: {} \nUniversity: {}"
+          .format(first_name,
+                  last_name,
+                  course,
+                  year_of_study,
+                  college,
+                  university))
+                                                        
+my_biodata()
