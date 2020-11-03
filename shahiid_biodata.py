e1d8824b21505430bc4126bd7628d57cfd37effb
Skiyaga <ashakykiyaga91@gmail.com>
2020-11-03 23:06:02 +0300

    add shahiid script

---
+++ shahiid_biodata.py
@@ -0,0 +1,8 @@
+#!/bin/python
+Biodata = {'Header': '..................SHAHIID KIYAGA BIO-DATA...................',
+                   'Name': 'SHAHIID KIYAGA', 'Title' : 'Student', 'Contact' : 'Mob: +256-702-966629 '
+                                                                              '\nEmail: ashakykiyaga91@gmail.com '
+                                                                              '\ngithub.com/Skiyaga', 
+'EduOneTitle': 'Master of Science in Bioinformatics \nMakerere University'}
+for values in Biodata.values():
+        print(values)
