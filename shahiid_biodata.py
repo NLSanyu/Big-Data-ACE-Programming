c6569933c87bcd357620a7564b0cdd9698a7b1b7
Skiyaga <ashakykiyaga91@gmail.com>
2020-11-07 16:05:33 +0300

    removed file

--- shahiid_biodata.py
+++
@@ -1,8 +0,0 @@
-
-Biodata = {'Header': '..................SHAHIID KIYAGA BIO-DATA...................',
-                   'Name': 'SHAHIID KIYAGA', 'Title' : 'Student', 'Contact' : 'Mob: +256-702-966629 '
-                                                                              '\nEmail: ashakykiyaga91@gmail.com '
-                                                                              '\ngithub.com/Skiyaga', 
-'EduOneTitle': 'Master of Science in Bioinformatics \nMakerere University'}
-for values in Biodata.values():
-        print(values)
